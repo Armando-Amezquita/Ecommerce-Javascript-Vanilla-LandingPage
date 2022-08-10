d9b9704d91efef2de19cabe4503c322a11c220d3
Armando Amezquita <amolina@smdigital.com.co>
2022-08-09 22:43:56 -0500

    integracion de archivo clase8 a index.js, modificacion de transiciones de despliegue de los menus y accion

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navbarEmail = document.querySelector('.navbar-email');
+const hambuguerMenu = document.querySelector('.menu');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navbarEmail.addEventListener('click', toggleNavbarMenu);
+hambuguerMenu.addEventListener('click', togglehambuguerMenu);
 
 function toggleNavbarMenu(){
-    desktopMenu.classList.toggle('inactive');
+    desktopMenu.classList.toggle('activeDesktopMenu');
+}
+
+function togglehambuguerMenu(){
+    mobileMenu.classList.toggle('activeHambuguerMenu');
 }
